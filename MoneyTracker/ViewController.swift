95831c036126b0a27464825877295b559dcd5326
equalnbgd <40184117+equalnbgd@users.noreply.github.com>
2020-11-12 23:28:18 +0100

    init

---
+++ MoneyTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyTracker
+//
+//  Created by Marko Bozilovic on 12/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
